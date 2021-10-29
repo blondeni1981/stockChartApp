273f8375c1d5c879e73bf66be691d9c73125e974
samcarson <carsonsam4@gmail.com>
2021-10-29 13:33:52 -0700

    Added styling and a header, needs routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthenticationComponent,
   ],
   imports: [
     BrowserModule,
