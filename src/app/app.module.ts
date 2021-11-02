1047294131664a6e075eccb33639253d572995af
blondeni1981 <blondeni1981@gmail.com>
2021-11-01 17:18:55 -0700

    added stock chart. It is not done. This is a rough draft with dummy data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {StockChartModule} from '@syncfusion/ej2-angular-charts';
+import {DateTimeService, CandleSeriesService} from '@syncfusion/ej2-angular-charts';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { StockchartComponent } from './stockchart/stockchart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AuthenticationComponent,
+    StockchartComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StockChartModule
   ],
-  providers: [],
+  providers: [DateTimeService, CandleSeriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
