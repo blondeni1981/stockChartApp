181e0c7b4b7338057a7db607888be5642611ef38
may <gaeul.kwon@bellevuecollege.edu>
2021-11-15 19:25:03 -0800

    Hide pages from non-customers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,29 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class AppComponent {
 
+  loggedIn: boolean = false;
+
   constructor(
     private route: ActivatedRoute,
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
    
     });
+
+    if( sessionStorage.getItem('ID:') == undefined ) {
+      this.loggedIn = false
+    }
+    // else if( sessionStorage.getItem('ID:').length < 5 ){
+    //   this.loggedIn = false;
+    //   console.log( sessionStorage.getItem('ID:') );
+    //   console.log(this.loggedIn);
+    // }
+    else {
+      this.loggedIn = true;
+    }
+    console.log(this.loggedIn);
   }
 
 }
